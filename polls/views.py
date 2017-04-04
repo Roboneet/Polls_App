5a79ebf3c43a4b5d6add961435c034184f54c584
Roboneet <neethumariyajoy@gmail.com>
2017-04-04 13:14:59 +0530

    models and views added

--- polls/views.py
+++ polls/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 
 def index(request):
 	return HttpResponse("Hello ,World! You are at the polls index! :D ")
+
+def detail(request,question_id):
+	return HttpResponse("You are looking at question %s."% question_id)
+
+def results(request,question_id):
+	return HttpResponse("You are looking at the results for the question %s"%question_id)
+
+def vote(request,question_id):
+	return HttpRespose("You're voting on question %s"%question_id)
